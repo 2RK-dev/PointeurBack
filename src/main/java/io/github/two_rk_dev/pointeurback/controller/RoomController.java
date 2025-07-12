80f1f1e48764a0e64774e48922fe8a5e33a8b8ed
rivon0507 <flavienrivon@gmail.com>
2025-07-12 18:25:11 +0300

    Add endpoint to retrieve available rooms

--- src/main/java/io/github/two_rk_dev/pointeurback/controller/RoomController.java
+++ src/main/java/io/github/two_rk_dev/pointeurback/controller/RoomController.java
@@ -1,8 +1,12 @@
 package io.github.two_rk_dev.pointeurback.controller;
 
-import io.github.two_rk_dev.pointeurback.dto.*;
+import io.github.two_rk_dev.pointeurback.dto.CreateRoomDTO;
+import io.github.two_rk_dev.pointeurback.dto.RoomDTO;
+import io.github.two_rk_dev.pointeurback.dto.UpdateRoomDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -30,6 +34,13 @@ public class RoomController {
 
     @DeleteMapping("/{roomId}")
     public ResponseEntity<Void> deleteRoom(@PathVariable Long roomId) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @GetMapping("/available")
+    public ResponseEntity<List<RoomDTO>> getAvailableRooms(@RequestParam LocalDateTime start,
+                                                           @RequestParam LocalDateTime end,
+                                                           @RequestParam(required = false) int size) {
         throw new UnsupportedOperationException("Not implemented");
     }
 }
