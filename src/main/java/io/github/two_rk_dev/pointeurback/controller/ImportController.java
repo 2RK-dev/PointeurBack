219f089d55661455cc4fcc367b7f0d6bbfa9a06b
Elyasnil <nilelyas@gmail.com>
2025-10-24 16:40:39 +0300

    Implémentation de la gestion d'erreur pour l'importation de plusieur fichier

--- src/main/java/io/github/two_rk_dev/pointeurback/controller/ImportController.java
+++ src/main/java/io/github/two_rk_dev/pointeurback/controller/ImportController.java
@@ -1,5 +1,6 @@
 package io.github.two_rk_dev.pointeurback.controller;
 
+import io.github.two_rk_dev.pointeurback.dto.datasync.ImportResponse;
 import io.github.two_rk_dev.pointeurback.exception.InvalidFileFormatException;
 import io.github.two_rk_dev.pointeurback.service.ImportService;
 import org.springframework.http.ResponseEntity;
@@ -26,5 +27,15 @@ public class ImportController {
             throw new InvalidFileFormatException("Corrupted file: %s or wrong content type".formatted(file.getOriginalFilename()), e);
         }
         return ResponseEntity.ok("File imported for entity: " + entityType);
+    }
+
+    @PostMapping("/batch-upload")
+    public ResponseEntity<ImportResponse> importMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        try {
+            ImportResponse response = importService.importMultipleFiles(files);
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            throw new InvalidFileFormatException("Batch import failed due to I/O error", e);
+        }
     }
 }
